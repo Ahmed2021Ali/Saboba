aec31e7d62094f257e091fa5cb275edb802e7c3d
Ahmed Ali Abdulghani <92600564+Ahmed2021Ali@users.noreply.github.com>
2024-10-14 18:37:08 +0300

    aa

--- database/migrations/2024_10_15_125509_create_education_table.php
+++ database/migrations/2024_10_15_125509_create_education_table.php
@@ -10,13 +10,11 @@ return new class extends Migration
     {
         Schema::create('education', function (Blueprint $table) {
             $table->id();
-            $table->string('university');
             $table->string('specialization');
             $table->string('university');
-            $table->string('end_date');
             $table->enum('employment_type', ['phd', 'master', 'without_certificate', 'diploma', 'college_student', 'high_school', 'grade_school']);
-            $table->string('end_date');
-            $table->string('start_date');
+            $table->date('end_date');
+            $table->date('start_date');
             $table->foreignId('job_profile_id')->references('id')->on('job_profiles')->cascadeOnUpdate()->cascadeOnDelete();
             $table->timestamps();
         });
