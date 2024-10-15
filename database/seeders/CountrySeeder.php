60ec676e0c02a0d04964ce0c254c00ae9921d8e2
Ahmed Abd Ellatif <uaahmed89@gmail.com>
2024-10-15 15:48:43 +0300

    make country seeder

--- database/seeders/CountrySeeder.php
+++ database/seeders/CountrySeeder.php
@@ -10,21 +10,31 @@ class CountrySeeder extends Seeder
 {
     public function run()
     {
-        // بيانات الدولة (هنا بس دولة واحدة)
-        $countryData = [
-            ['locale' => 'en', 'name' => 'Egypt'],
-            ['locale' => 'ar', 'name' => 'مصر'],
-            ['locale' => 'fr', 'name' => 'Égypte'],
+        // بيانات الدول
+        $countriesData = [
+            [
+                ['locale' => 'en', 'name' => 'Egypt'],
+                ['locale' => 'ar', 'name' => 'مصر'],
+                ['locale' => 'fr', 'name' => 'Égypte'],
+            ],
+            [
+                ['locale' => 'en', 'name' => 'Palestine'],
+                ['locale' => 'ar', 'name' => 'فلسطين'],
+                ['locale' => 'fr', 'name' => 'Palestine'],
+            ],
         ];
 
-        // إنشاء الدولة الجديدة
-        $country = Country::create(); // إنشاء الدولة بدون اسم
+        // إنشاء الدول الجديدة
+        foreach ($countriesData as $countryData) {
+            // إنشاء الدولة الجديدة
+            $country = Country::create(); // إنشاء الدولة بدون اسم
+            
+            // إضافة الترجمات للبلد
+            foreach ($countryData as $data) {
+                $country->translateOrNew($data['locale'])->name = $data['name'];
+            }
 
-        // إضافة الترجمات للبلد
-        foreach ($countryData as $data) {
-            $country->translateOrNew($data['locale'])->name = $data['name'];
+            $country->save();
         }
-
-        $country->save();
     }
 }
