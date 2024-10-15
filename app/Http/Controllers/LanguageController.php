8f3bff4ae254db827f38e7a22dccb3a57a2a1800
Ahmed Ali Abdulghani <92600564+Ahmed2021Ali@users.noreply.github.com>
2024-10-15 12:32:47 +0300

    dd

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -8,33 +8,10 @@ use Illuminate\Support\Facades\Auth;
 
 class LanguageController extends Controller
 {
-    /*  Show All Size  */
     public function index()
     {
         $languages = Auth::User()->userLanguages();
         return response()->json(LanguageResource::collection($languages));
     }
-
-    /*  Store Size  */
-
-/*    public function store(StoreSize $request)
-    {
-        $size = Size::create($request->validated());
-        return response()->json(['message' => 'تم اضافة المقاس بنجاح',
-            'size' => new SizeResource($size)
-        ]);
-    }
-
-    public function update(UpdateSize $request, Size $size)
-    {
-        $size->update($request->validated());
-        return response()->json(['message' => 'تم التحديث اسم المقاس بنجاح', 'size' => new SizeResource($size)
-        ]);
-    }
-
-    public function destroy(Size $size)
-    {
-        $size->delete();
-        return response()->json(['message' => 'تم حذف المقاس بنجاح']);
-    }*/
+    
 }
