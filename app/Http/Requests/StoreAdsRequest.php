1f02ef2ba83fe3d5ae507b5b2ef5af0883b5f0bf
Ahmed Abd Ellatif <uaahmed89@gmail.com>
2024-10-19 15:25:13 +0300

    enhance store ad method

--- app/Http/Requests/StoreAdsRequest.php
+++ app/Http/Requests/StoreAdsRequest.php
@@ -21,9 +21,10 @@ class StoreAdsRequest extends FormRequest
             'category_id' => 'required|exists:categories,id',
             'city_id' => 'required|exists:cities,id',
             'image' => 'nullable|image',
-            'locale' => 'required|string',
-            'name' => 'required|string|max:255',
-            'description' => 'required|string|max:1000',
+            'translations' => 'nullable|array',
+            'translations.*.locale' => 'required|string',
+            'translations.*.name' => 'required|string|max:255',
+            'translations.*.description' => 'required|string|max:1000',
         ];
     }
 
@@ -40,20 +41,20 @@ class StoreAdsRequest extends FormRequest
             'city_id.required' => 'Please enter the city ID.',
             'city_id.exists' => 'The city ID does not exist.',
             'image.image' => 'The image must be a valid image file.',
-            'locale.required' => 'Please enter the locale.',
-            'locale.string' => 'The locale must be a string.',
-            'name.required' => 'Please enter the advertisement name.',
-            'name.string' => 'The name must be a string.',
-            'name.max' => 'The name may not be greater than 255 characters.',
-            'description.required' => 'Please enter the advertisement description.',
-            'description.string' => 'The description must be a string.',
-            'description.max' => 'The description may not be greater than 1000 characters.',
+            'translations.array' => 'The translations must be an array.',
+            'translations.*.locale.required' => 'Please enter the locale.',
+            'translations.*.locale.string' => 'The locale must be a string.',
+            'translations.*.name.required' => 'Please enter the advertisement name.',
+            'translations.*.name.string' => 'The name must be a string.',
+            'translations.*.name.max' => 'The name may not be greater than 255 characters.',
+            'translations.*.description.required' => 'Please enter the advertisement description.',
+            'translations.*.description.string' => 'The description must be a string.',
+            'translations.*.description.max' => 'The description may not be greater than 1000 characters.',
         ];
     }
 
     protected function failedValidation(Validator $validator)
     {
-        // Throw an HttpResponseException with a custom JSON error response
         throw new HttpResponseException(response()->json([
             'success' => false,
             'message' => 'Validation failed',
