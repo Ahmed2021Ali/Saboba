f0eddb54374dd5ab069d79975f8fbccc6c92b1a5
Ahmed Ali Abdulghani <92600564+Ahmed2021Ali@users.noreply.github.com>
2024-10-19 21:30:38 +0300

    111

--- app/Http/Controllers/SkillsController.php
+++ app/Http/Controllers/SkillsController.php
@@ -7,16 +7,16 @@ use App\Http\Requests\StoreSkillsRequest;
 use App\Http\Requests\UpdateSkillsRequest;
 use App\Http\Resources\SkillsResource;
 use App\Models\Skills;
+use App\Traits\ApiResponseTrait;
 use Illuminate\Support\Facades\Auth;
 
 class SkillsController extends Controller
 {
+    use ApiResponseTrait;
+
     public function index()
     {
-        return response()->json([
-            'message' => ' User Skills',
-            'skills' => SkillsResource::collection(Auth::User()->userSkills),
-        ], 201);
+        return $this->successResponse(SkillsResource::collection(Auth::User()->userSkills), 'User Skills', 200);
     }
 
     public function store(StoreSkillsRequest $request)
@@ -24,15 +24,10 @@ class SkillsController extends Controller
         foreach ($request->skills_id as $skill_id) {
             $skill = Auth::User()->userSkills()->where('skills_id', $skill_id)->first();
             if ($skill) {
-                return response()->json([
-                    'message' => 'your Language already exists',
-                ], 201);
+                return $this->successResponse(null, 'your Language already exists', 200);
             } else {
                 Auth::User()->userSkills()->attach($skill_id);
-
-                return response()->json([
-                    'message' => 'your Language created successfully',
-                ], 201);
+                return $this->successResponse(null, 'your Language created successfully', 201);
             }
         }
     }
@@ -40,5 +35,7 @@ class SkillsController extends Controller
     public function destroy($id)
     {
         Auth::User()->userskills()->detach($id);
+        return $this->successResponse(null, 'your Language deleted successfully', 201);
     }
+
 }
