f108c56b8c069ccd2b121bf3bd7f3fff25e2cf51
Ahmed Abd Ellatif <uaahmed89@gmail.com>
2024-10-23 14:16:04 +0300

    test response

--- app/Http/Controllers/AdsController.php
+++ app/Http/Controllers/AdsController.php
@@ -76,7 +76,7 @@ class AdsController extends Controller
 
     public function store(Request $request)
     {
-        dd($request['translations'][0]);
+        dd($request['translations'][1]);
         return DB::transaction(function () use ($request) {
             // 1. Create the Ad
             $ad = $this->createAd($request);
