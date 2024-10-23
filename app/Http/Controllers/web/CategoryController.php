1cdac3f7cc726e84fd145f36f8cc0094c810c2d9
Ahmed Ali Abdulghani <92600564+Ahmed2021Ali@users.noreply.github.com>
2024-10-23 19:19:05 +0300

    sssssssss

--- app/Http/Controllers/web/CategoryController.php
+++ app/Http/Controllers/web/CategoryController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\web;
 
 use App\Http\Controllers\Controller;
+use App\Http\Traits\media;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+    use media;
+
     public function index(Request $request)
     {
         $categories = Category::with('translations')->whereNull('parent_id')->paginate(8);
@@ -16,8 +19,9 @@ class CategoryController extends Controller
 
     public function store(Request $request)
     {
-        $categories = Category::with('translations')->whereNull('parent_id')->paginate(8);
-        return view('dashboard.categories.index', compact('categories'));
+        $DataValidated = $request->validate(['name' => 'required|string']);
+        Category::create($DataValidated);
+        return to_route('categories.index');
     }
 
 }
