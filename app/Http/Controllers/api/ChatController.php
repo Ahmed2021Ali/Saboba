3a03edc29587fa3cf1488b12d7b39a841feb5796
Ahmed Ali Abdulghani <92600564+Ahmed2021Ali@users.noreply.github.com>
2024-10-27 23:23:09 +0300

    aaaaaaaaa

--- app/Http/Controllers/api/ChatController.php
+++ app/Http/Controllers/api/ChatController.php
@@ -18,6 +18,7 @@ class ChatController extends Controller
 
     public function show()
     {
+        dd(Auth::id());
         $chats = Chat::where('receiver_id', Auth::id())
             ->Orwhere('sender_id', Auth::id())->get();
         return response()->json(['Data'=>new ChatResource($chats), 'success' => 'Your Chats'],200);
