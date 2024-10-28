78edd27563f000bfff6882326cd60b6433064a0a
Ahmed Ali Abdulghani <92600564+Ahmed2021Ali@users.noreply.github.com>
2024-10-29 00:32:05 +0300

    سسسسسسسس

--- app/Http/Controllers/web/CountryController.php
+++ app/Http/Controllers/web/CountryController.php
@@ -16,22 +16,30 @@ class CountryController extends Controller
 
     public function store(Request $request)
     {
-        $validationData = $request->validate(['name' => 'nullable|string']);
-        Country::create($validationData);
-
-
+        $validationData = $request->validate(['name' => 'required|string', 'image' => 'required|image|max:10000']);
+        $country = Country::create($validationData);
+        if (isset($validationData['image'])) {
+            $country->addMedia($validationData['image'])->toMediaCollection('countryImages');
+        }
+        flash()->success('تم اضافة الدولة  بنجاح');
+        return redirect()->route('dashboard.country.index');
     }
 
     public function update(Request $request, Country $country)
     {
-        $validationData = $request->validate(['name' => 'nullable|string']);
+        $validationData = $request->validate(['name' => 'nullable|string', 'image' => 'nullable|image|max:10000']);
         $country->update($validationData);
-
+        if (isset($validationData['image'])) {
+            $country->addMedia($validationData['image'])->toMediaCollection('countryImages');
+        }
+        flash()->success('تم تحديث الدولة  بنجاح');
+        return redirect()->route('dashboard.country.index');
     }
 
     public function destroy(Country $country)
     {
         $country->delete();
-
+        flash()->success('تم حذف الدولة  بنجاح');
+        return redirect()->route('dashboard.country.index');
     }
 }
--- resources/views/dashboard/country/index.blade.php
+++ resources/views/dashboard/country/index.blade.php
@@ -9,68 +9,87 @@
 
 @section('content')
     <main role="main" class="main-content">
+        <div class="container-fluid">
+            <div class="row justify-content-center">
+                <div class="col-12">
+                    <h2 class="mb-2 page-title"> اضافة دولة جديد</h2>
+                    @if (session('success'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('success') }}
+                        </div>
+                    @endif
+                    <div class="pull-right mb-2">
+                        <br>
+                        <button type="button" class="btn mb-2 btn-outline-secondary" data-toggle="modal"
+                                data-target="#varyModal" data-whatever="@mdo">اضافة دولة جديد
+                        </button>
+                        @include('dashboard.country.create')
+                    </div>
 
-        @if($countries->IsNotempty())
-            <h1 class="text-center"> جميع الدول </h1>
-            @if (session('success'))
-                <div class="alert alert-success" role="alert">
-                    {{ session('success') }}
-                </div>
-            @endif
-            <div class="col-md-6 col-xl-3 mb-4">
-                <br>
-                <button type="button" class="btn mb-4 btn-outline-secondary" data-toggle="modal"
-                        data-target="#varyModal" data-whatever="@mdo">  اضافة دوله جدبدة
-                </button>
-                @include('dashboard.country.create')
-            </div>
-            <br>
+                    @if($countries->IsNotempty())
 
-            <div class="row">
-                @foreach($countries as $country)
-                    <div class="col-md-6 col-xl-3 mb-4">
-                        <div class="card shadow bg-primary text-white">
-                            <div class="card-body">
-                                <div class="row align-items-center">
-                                    <div class="col-3 text-center">
+                        <div class="row my-4">
+                            <div class="col-md-12">
+                                <div class="card shadow">
+                                    <div class="card-body">
+                                        <table class="table datatables" id="dataTable-1">
+                                            <thead>
+                                            <tr>
+                                                <th></th>
+                                                <th>  اسم الدولة </th>
+                                            </tr>
+                                            </thead>
+                                            <tbody>
+                                            @foreach ($countries as $key => $country)
+                                                <tr>
+                                                    <td>{{ ++$key }}</td>
 
+                                                    <td> {{ $country->name }}</td>
+                                                    <td>
+                                                        <button class="btn btn-sm btn-success" data-toggle="modal"
+                                                                data-target="#imageModal_{{$country->id}}">
+                                                            <i class="fa fa-edit"></i> عرض المرفقات
+                                                        </button>
+                                                        @include('dashboard.images.index', ['model' => $country, 'folder' => 'countryImages'])
+
+                                                    </td>
+                                                    <td>
+
+{{--                                                        <a class="btn btn-sm btn-warning" data-toggle="modal"
+                                                           data-target="#edit_latest_news_{{$latestNew->id}}"
+                                                           data-whatever="@mdo"><i class="fa-solid fa-pen-to-square"></i>
+                                                            تعديل</a>
+                                                        @include('dashboard.latest_news.edit',['latest_news'=>$latestNew,'category'=>$category])
+
+
+                                                        <button type="button" class="btn btn-sm btn-danger"
+                                                                data-toggle="modal"
+                                                                data-target="#delete_latest_news_{{$latestNew->id}}"><i
+                                                                class="fa-solid fa-trash"></i> حذف
+                                                        </button>
+                                                        @include('dashboard.latest_news.delete',['latest_news'=>$latestNew,])--}}
+
+
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                            </tbody>
+                                        </table>
                                         {{--
-                                                                                <a href="{{route('getNews',$country->id)}}">
+                                                                            {!! $subAddresses->links/*('pagination::bootstrap-5')*/ !!}
                                         --}}
-
-                                        <span class="circle circle-sm bg-primary-light">
-                                        <i class="fe fe-16 fe-shopping-bag text-white mb-0"></i>
-                                      </span>
-
-                                        {{--
-                                                                                </a>
-                                        --}}
-
                                     </div>
-
-                                    {{--
-                                                                        <a href="{{route('getNews',$category->id)}}">
-                                    --}}
-
-                                    <div class="col pr-0">
-                                        <h4 class="text-light mb-0">{{$country->name}}</h4>
-                                    </div>
-
-                                    {{--
-                                                                        </a>
-                                    --}}
-
                                 </div>
                             </div>
                         </div>
-                    </div>
-                @endforeach
 
+                    @else
+                        <h1 style="text-align: center"> لا يوجد دول مضافة  </h1>
+                    @endif
+                    <p class="text-center text-primary"><small>دليل من ItSolutionStuff.com</small></p>
+                </div>
             </div>
-        @else
-            <h1 style="text-align: center"> لا يوجد دول </h1>
-        @endif
-
+        </div>
     </main>
 @endsection
 
