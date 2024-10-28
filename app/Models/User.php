026e89b0acf05c4d7f47b8d7ac6396ef5d3c6cdf
Ahmed Ali Abdulghani <92600564+Ahmed2021Ali@users.noreply.github.com>
2024-10-28 15:28:05 +0300

    sssssss

--- app/Models/User.php
+++ app/Models/User.php
@@ -85,6 +85,6 @@ class User extends Authenticatable implements JWTSubject, HasMedia
 
     public function identifyVerification()
     {
-        return $this->hasOne(CompanyIdentityVerification::class)->select('id', 'status')->where('type', 'company')->first();
+        return $this->hasOne(CompanyIdentityVerification::class);
     }
 }
