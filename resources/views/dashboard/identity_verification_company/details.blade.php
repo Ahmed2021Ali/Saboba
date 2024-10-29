113f41d5ebc2b69671ad90a675f98340fed1ddf9
Ahmed Ali Abdulghani <92600564+Ahmed2021Ali@users.noreply.github.com>
2024-10-29 19:13:15 +0300

    يييييييي

--- resources/views/dashboard/identity_verification_company/details.blade.php
+++ resources/views/dashboard/identity_verification_company/details.blade.php
@@ -2,7 +2,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="varyModalLabel"> محتوي البوست </h5>
+                <h5 class="modal-title" id="varyModalLabel">  تفاصيل الموسسة  </h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">×</span>
                 </button>
@@ -10,12 +10,12 @@
             <div class="modal-body">
                 <div class="form-group">
                     <div class="scrollable">
-                        <h4> اسم الموسسة : {{$verification->user->name}} </h4>
-                        <h4> الاميل الموسسة : {{$verification->user->email}} </h4>
-                        <h4> نبذة الموسسة : {{$verification->user->overview}} </h4>
-                        <h4> دولة الموسسة : {{$verification->user->country->name}} </h4>
-                        <h4> رقم الهاتف الموسسة : {{$verification->user->contact_number}} </h4>
-                        <h4> رقم الواتس الموسسة : {{$verification->user->whatsapp_number}} </h4>
+                        <h4> اسم  : {{$verification->user->name}} </h4>
+                        <h4> الاميل  : {{$verification->user->email}} </h4>
+                        <h4> نبذة  : {{$verification->user->overview}} </h4>
+                        <h4> دولة  : {{$verification->user->country->name}} </h4>
+                        <h4> رقم الهاتف  : {{$verification->user->contact_number}} </h4>
+                        <h4> رقم الواتس  : {{$verification->user->whatsapp_number}} </h4>
 
                     </div>
                 </div>
