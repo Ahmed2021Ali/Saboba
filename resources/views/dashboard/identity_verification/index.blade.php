acca66f6efe6d43bf4686186dee7d640a7f55ff2
Ahmed Ali Abdulghani <92600564+Ahmed2021Ali@users.noreply.github.com>
2024-10-29 17:07:29 +0300

    ييييييييي

--- resources/views/dashboard/identity_verification/index.blade.php
+++ resources/views/dashboard/identity_verification/index.blade.php
@@ -12,19 +12,12 @@
         <div class="container-fluid">
             <div class="row justify-content-center">
                 <div class="col-12">
-                    <h2 class="mb-2 page-title text-center" style="text-align: center"> اضافة دولة جديد</h2>
+                    <h2 class="mb-2 page-title text-center" style="text-align: center"> اثبات هوية الشركات</h2>
                     @if (session('success'))
                         <div class="alert alert-success" role="alert">
                             {{ session('success') }}
                         </div>
                     @endif
-                    <div class="pull-right mb-2">
-                        <br>
-                        <button type="button" class="btn mb-2 btn-outline-secondary" data-toggle="modal"
-                                data-target="#varyModal" data-whatever="@mdo">اضافة دولة جديد
-                        </button>
-                        @include('dashboard.identity_verification.create')
-                    </div>
 
                     @if($companies->IsNotempty())
 
@@ -36,37 +29,39 @@
                                             <thead>
                                             <tr>
                                                 <th></th>
-                                                <th>  اسم الدولة </th>
+                                                <th> اسم الموسسة</th>
+                                                <th> حالة الموسسة</th>
+
                                             </tr>
                                             </thead>
                                             <tbody>
                                             @foreach ($companies as $key => $company)
                                                 <tr>
                                                     <td>{{ ++$key }}</td>
-
-                                                    <td> {{ $company->status }}</td>
+                                                    <td> {{ $company->user->name }}</td>
+                                                    <td> {{ $company->status ===1 ? " تم اثبات ملكية الحساب " : " لم يتم الاثبات الملكية حتي الان " }}</td>
                                                     <td>
 
-{{--
-                                                        <a class="btn btn-sm btn-info" href="{{ route('country.show', $country) }}">
-                                                            <i class="fa-solid fa-list"></i>  عرض المدن لهذه الدولة
-                                                        </a>
+                                                        {{--
+                                                                                                                <a class="btn btn-sm btn-info" href="{{ route('country.show', $country) }}">
+                                                                                                                    <i class="fa-solid fa-list"></i>  عرض المدن لهذه الدولة
+                                                                                                                </a>
 
-                                                       <a class="btn btn-sm btn-warning" data-toggle="modal"
-                                                           data-target="#edit_latest_news_{{$country->id}}"
-                                                           data-whatever="@mdo"><i class="fa-solid fa-pen-to-square"></i>
-                                                            تعديل</a>
-                                                        @include('dashboard.country.edit',['country'=>$country])
+                                                                                                               <a class="btn btn-sm btn-warning" data-toggle="modal"
+                                                                                                                   data-target="#edit_latest_news_{{$country->id}}"
+                                                                                                                   data-whatever="@mdo"><i class="fa-solid fa-pen-to-square"></i>
+                                                                                                                    تعديل</a>
+                                                                                                                @include('dashboard.country.edit',['country'=>$country])
 
 
-                                                        <button type="button" class="btn btn-sm btn-danger"
-                                                                data-toggle="modal"
-                                                                data-target="#delete_latest_news_{{$country->id}}"><i
-                                                                class="fa-solid fa-trash"></i> حذف
-                                                        </button>
-                                                        @include('dashboard.country.delete',['country'=>$country,])
+                                                                                                                <button type="button" class="btn btn-sm btn-danger"
+                                                                                                                        data-toggle="modal"
+                                                                                                                        data-target="#delete_latest_news_{{$country->id}}"><i
+                                                                                                                        class="fa-solid fa-trash"></i> حذف
+                                                                                                                </button>
+                                                                                                                @include('dashboard.country.delete',['country'=>$country,])
 
---}}
+                                                        --}}
 
                                                     </td>
                                                 </tr>
@@ -82,7 +77,7 @@
                         </div>
 
                     @else
-                        <h1 style="text-align: center"> لا يوجد دول مضافة  </h1>
+                        <h1 style="text-align: center"> لا يوجد دول مضافة </h1>
                     @endif
                     <p class="text-center text-primary"><small>دليل من ItSolutionStuff.com</small></p>
                 </div>
