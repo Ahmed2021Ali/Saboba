d42317f4009e20c86f45ccd183a7846b0ca024b0
Ahmed Ali Abdulghani <92600564+Ahmed2021Ali@users.noreply.github.com>
2024-10-29 20:27:52 +0300

    sssssssssssssssssssss

--- app/Http/Controllers/IdentityVerificationController.php
+++ app/Http/Controllers/IdentityVerificationController.php
@@ -11,15 +11,25 @@ class IdentityVerificationController extends Controller
     public function index()
     {
         $verifications = CompanyIdentityVerification::where('status', 0)->get();
+        $users_id = CompanyIdentityVerification::where('status', 1)->pluck('user_id');
         return view('dashboard.identity_verification_company.index', [
             'verifications' => $verifications,
-            'companies' => User::where('type', 'company')->get()
-        ]);
+            'companies' => User::where('type', 'company')->whereIn('id', '!=', $users_id)->get()]);
     }
 
     public function store(Request $request)
     {
         $validationData = $request->validate(['company_id' => 'nullable|exists:users,id']);
+        $company = CompanyIdentityVerification::where('user_id', $validationData['company_id'])->first();
+        if ($company) {
+            if ($company->status == 1) {
+                flash()->error('  تم اثبات ملكية هذ الموسسة من قبل  ');
+                return redirect()->back();
+            } else {
+                flash()->success('  تم اثبات ملكية الموسسة بنجاح ');
+                return redirect()->back();
+            }
+        }
         CompanyIdentityVerification::create(['user_id' => $validationData['company_id'], 'status' => 1]);
         flash()->success('  تم اثبات ملكية الموسسة بنجاح ');
         return redirect()->back();
