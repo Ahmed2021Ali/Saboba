fde351b8ab0f80b9b0f3bf598fda768faa15854a
Ahmed Ali Abdulghani <92600564+Ahmed2021Ali@users.noreply.github.com>
2024-10-29 18:18:32 +0300

    يييي
    ييييييييييييي

---
+++ resources/views/dashboard/identity_verification_company/accept.blade.php
@@ -0,0 +1,25 @@
+<div class="modal fade" id="accept_verification_{{$verification->id}}" tabindex="-1" role="dialog"
+     aria-labelledby="varyModalLabel" style="display: none;" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="varyModalLabel"> رفض اثبات الهوية </h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form action="{{route('verifications.update',$verification)}}" method="post" enctype="multipart/form-data">
+                    @method('put')
+                    @csrf
+
+
+                    <div class="modal-footer">
+                        <button type="submit" class="btn mb-2 btn-primary">  قبول اثبات الهوية  </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
