2295b3807531ca9060301560c9dbfbf62312c26f
Ahmed Ali Abdulghani <92600564+Ahmed2021Ali@users.noreply.github.com>
2024-10-30 21:34:02 +0300

    sssssss

--- resources/lang/en/admin_dashboard/sidebar/messages.php
+++ resources/lang/en/admin_dashboard/sidebar/messages.php
@@ -30,24 +30,5 @@ return [
     'block_users' => 'Block users  ',
 
 
-
-
-
-// Login Message
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
-
-// Show Message
-    'add' => 'The data has been saved successfully',
-    'edit' => 'The data has been modified successfully',
-    'delete' => 'The data has been deleted successfully',
-
-// validation Message
-    'required' => 'The  field is required.',
-    'string' => 'The  field is string ',
-    'min' => 'The  field must be at least :n characters  ',
-    'max' => 'The name field must not be greater than :n characters',
-    'email' => 'The email field must be a valid email address',
-    'confirmed' => 'The password field confirmation does not match'
+    
 ];
