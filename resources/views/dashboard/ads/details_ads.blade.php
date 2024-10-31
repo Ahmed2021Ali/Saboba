bcd86bf14928e6c1b82695a3902320cb55e2ddd1
Ahmed Ali Abdulghani <92600564+Ahmed2021Ali@users.noreply.github.com>
2024-10-31 15:03:30 +0300

    dddddddddd

--- resources/views/dashboard/ads/details_ads.blade.php
+++ resources/views/dashboard/ads/details_ads.blade.php
@@ -37,7 +37,7 @@
                                     <strong class="card-title">{{$ad->name}}</strong>
                                     <span class="float-right">
                                         <i class="fe fe-flag mr-2"></i>
-                                        <span class="badge badge-pill @if($ad->status ===0) badge-danger @else badge-success  @endif text-white"  {{ $ad->status ===0 ?  __('admin_dashboard/ads/messages.not_approve')  : __('admin_dashboard/ads/messages.approve') }}></span>
+                                        <span class="badge badge-pill @if($ad->status ===0) badge-danger @else badge-success  @endif text-white"> {{ $ad->status ===0 ?  __('admin_dashboard/ads/messages.not_approve')  : __('admin_dashboard/ads/messages.approve') }}</span>
                                     </span>
                                 </div>
                                 <div class="card-body">
