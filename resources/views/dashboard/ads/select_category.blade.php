d192a83b1906e6af3c75017c732340b844dde9ca
Ahmed Ali Abdulghani <92600564+Ahmed2021Ali@users.noreply.github.com>
2024-10-31 17:59:17 +0300

    يييييييييييي

--- resources/views/dashboard/ads/select_category.blade.php
+++ resources/views/dashboard/ads/select_category.blade.php
@@ -4,7 +4,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="editCategoryModalLabel"> اشعار بالتعديل </h5>
+                <h5 class="modal-title" id="editCategoryModalLabel"> القسم الذي تريد اضافة اعلان فيه </h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -26,7 +26,7 @@
 
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">إغلاق</button>
-                    <button type="submit" class="btn btn-primary"> التأكيد الارسال</button>
+                    <button type="submit" class="btn btn-primary">  اضافة الاعلان</button>
                 </div>
             </form>
         </div>
