7f40b7fee47748493fde846a9e59a20ee7c492fe
Ahmed Ali Abdulghani <92600564+Ahmed2021Ali@users.noreply.github.com>
2024-10-31 23:35:30 +0300

    dddddddddddd

--- resources/lang/en/admin_dashboard/city/messages.php
+++ resources/lang/en/admin_dashboard/city/messages.php
@@ -9,7 +9,8 @@ return [
     'name' => 'name',
     'show_image' => 'show image',
     'no_cities_added'=>'No cities added',
-    'do_you_want_to_delete_this_city'=>'Do you want to delete this city ?'
+    'do_you_want_to_delete_this_city'=>'Do you want to delete this city ?',
+    'operation' => 'Operation',
 
 
 ];
