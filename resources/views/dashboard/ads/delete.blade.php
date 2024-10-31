f6009d487a6fba02ac6100cd2400165c277eeb4b
Ahmed Ali Abdulghani <92600564+Ahmed2021Ali@users.noreply.github.com>
2024-10-31 17:14:52 +0300

    ققققق

--- resources/views/dashboard/ads/delete.blade.php
+++ resources/views/dashboard/ads/delete.blade.php
@@ -2,15 +2,18 @@
 <div class="modal fade" id="deleteCategoryModal{{ $ad->id }}" tabindex="-1" role="dialog" aria-labelledby="deleteCategoryModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
+
             <div class="modal-header">
                 <h5 class="modal-title" id="deleteCategoryModalLabel">{{ __('admin_dashboard/ads/messages.reject') }}</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
+
             <div class="modal-body">
                 {{ __('admin_dashboard/ads/messages.sure_delete') }} <span style="color: red">{{ $ad->name }}</span>
             </div>
+
             <div class="modal-footer">
                 <form action="{{ route('ads.destroy', $ad) }}" method="POST">
                     @csrf
