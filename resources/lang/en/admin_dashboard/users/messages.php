96a2108ed51f0f39a326aacc8706485896e6a534
Ahmed Ali Abdulghani <92600564+Ahmed2021Ali@users.noreply.github.com>
2024-10-31 20:08:41 +0300

    kkkkkkkkkkkk

--- resources/lang/en/admin_dashboard/users/messages.php
+++ resources/lang/en/admin_dashboard/users/messages.php
@@ -6,6 +6,8 @@ return [
     'name' => 'Name',
     'email' => 'Email',
     'password' => 'Password',
+    'password_confirmation'=>'Password Confirmation',
+
     'phone_number' => 'Phone Number',
     'roles' => 'Role',
     'country' => 'Country',
@@ -15,8 +17,12 @@ return [
     'edit'=>'Edit User',
     'delete'=>'Delete User',
     'add'=>'Add User',
-    'no_data'=>'There is no data to display..'
-
-
+    'no_data'=>'There is no data to display..',
+    'select_country'=>' Select Country ',
+    'type'=>' Type User',
+    'select_type'=>' Select Type User ',
+    'user'=>'User',
+    'admin'=>'Admin',
+    'company'=>'Company'
 
 ];
--- resources/views/dashboard/users/create.blade.php
+++ resources/views/dashboard/users/create.blade.php
@@ -7,13 +7,7 @@
         <div class="container-fluid">
             <div class="row justify-content-center">
                 <div class="col-12">
-                    <h2 class="page-title">إنشاء مستخدم جديد</h2> <!-- Page title in Arabic -->
-                    <div class="text-muted mb-4">
-                        <a class="btn btn-primary btn-sm mb-2" href="{{ route('users.index') }}">
-                            <i class="fa fa-arrow-left"></i> العودة
-                        </a>
-                    </div>
-
+                    <h2 class="page-title">{{ __('admin_dashboard/users/messages.add') }}</h2> <!-- Page title in Arabic -->
                     <!-- Display Errors -->
                     @if (count($errors) > 0)
                         <div class="alert alert-danger">
@@ -35,10 +29,11 @@
                             <form method="POST" action="{{ route('users.store') }}" enctype="multipart/form-data">
                                 @csrf
                                 <div class="row">
+
                                     <!-- Name Input -->
                                     <div class="col-md-12">
                                         <div class="form-group mb-3">
-                                            <label for="userName"><strong>الاسم:</strong></label>
+                                            <label for="userName"><strong>{{ __('admin_dashboard/users/messages.name') }}:</strong></label>
                                             <input type="text" name="name" id="userName" placeholder="الاسم"
                                                    class="form-control" value="{{ old('name') }}">
                                         </div>
@@ -47,7 +42,7 @@
                                     <!-- Email Input -->
                                     <div class="col-md-12">
                                         <div class="form-group mb-3">
-                                            <label for="userEmail"><strong>البريد الإلكتروني:</strong></label>
+                                            <label for="userEmail"><strong>{{ __('admin_dashboard/users/messages.email') }}:</strong></label>
                                             <input type="email" name="email" id="userEmail"
                                                    placeholder="البريد الإلكتروني" class="form-control"
                                                    value="{{ old('email') }}">
@@ -57,7 +52,7 @@
                                     <!-- Phone Number Input -->
                                     <div class="col-md-12">
                                         <div class="form-group mb-3">
-                                            <label for="phone"><strong>رقم الهاتف:</strong></label>
+                                            <label for="phone"><strong>{{ __('admin_dashboard/users/messages.phone_number') }} :</strong></label>
                                             <input type="text" name="phone" id="phone"
                                                    placeholder="رقم الهاتف" class="form-control"
                                                    value="{{ old('phone_number') }}">
@@ -67,7 +62,7 @@
                                     <!-- Password Input -->
                                     <div class="col-md-12">
                                         <div class="form-group mb-3">
-                                            <label for="password"><strong>كلمة المرور:</strong></label>
+                                            <label for="password"><strong> {{ __('admin_dashboard/users/messages.password') }}:</strong></label>
                                             <input type="password" name="password" id="password"
                                                    placeholder="كلمة المرور" class="form-control">
                                         </div>
@@ -76,8 +71,7 @@
                                     <!-- Confirm Password Input -->
                                     <div class="col-md-12">
                                         <div class="form-group mb-3">
-                                            <label for="password_confirmation"><strong>تأكيد كلمة
-                                                    المرور:</strong></label>
+                                            <label for="password_confirmation"><strong>{{ __('admin_dashboard/users/messages.password_confirmation') }}  :</strong></label>
                                             <input type="password" name="password_confirmation"
                                                    id="password_confirmation" placeholder="تأكيد كلمة المرور"
                                                    class="form-control">
@@ -87,9 +81,9 @@
                                     <!-- Country Selection -->
                                     <div class="col-md-12">
                                         <div class="form-group mb-3">
-                                            <label for="country_id"><strong>اختر دولة  :</strong></label>
+                                            <label for="country_id"><strong> {{ __('admin_dashboard/users/messages.country') }}  :</strong></label>
                                             <select name="country_id" id="country_id" class="form-control" required>
-                                                <option style="display: none"> اختر دولة المستخدم </option>
+                                                <option style="display: none"> {{ __('admin_dashboard/users/messages.select_country') }} </option>
                                                 @foreach($countries as $country)
                                                     <option value="{{$country->id}}"> {{$country->name}}</option>
                                                 @endforeach
@@ -100,12 +94,12 @@
                                     <!-- Type Selection -->
                                     <div class="col-md-12">
                                         <div class="form-group mb-3">
-                                            <label for="type"><strong>النوع :</strong></label>
+                                            <label for="type"><strong>{{ __('admin_dashboard/users/messages.type') }} :</strong></label>
                                             <select name="type" id="type" class="form-control">
-                                                <option style="display: none"> اختر نوع المستخدم </option>
-                                                <option value="admin"> ادمن</option>
-                                                <option value="personal"> مستخدم</option>
-                                                <option value="company"> شركة</option>
+                                                <option style="display: none"> {{ __('admin_dashboard/users/messages.select_type') }} </option>
+                                                <option value="admin"> {{ __('admin_dashboard/users/messages.admin') }}</option>
+                                                <option value="personal"> {{ __('admin_dashboard/users/messages.user') }}</option>
+                                                <option value="company"> {{ __('admin_dashboard/users/messages.company') }}</option>
                                             </select>
                                         </div>
                                     </div>
