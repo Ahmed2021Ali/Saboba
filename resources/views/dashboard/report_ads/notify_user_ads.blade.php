44e36e8f4e7683ee72faa24eef59b6d8c4262a7b
Ahmed Ali Abdulghani <92600564+Ahmed2021Ali@users.noreply.github.com>
2024-11-01 15:59:03 +0200

    dddd
    dddddddddddddddddd

--- resources/views/dashboard/report_ads/notify_user_ads.blade.php
+++ resources/views/dashboard/report_ads/notify_user_ads.blade.php
@@ -4,7 +4,7 @@
         <div class="modal-content">
 
             <div class="modal-header">
-                <h5 class="modal-title" id="deleteCategoryModalLabel">{{ __('admin_dashboard/report/messages.Block the advertiser user') }}</h5>
+                <h5 class="modal-title" id="deleteCategoryModalLabel">{{ __('admin_dashboard/report/messages.Advertiser warning message') }}</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -13,7 +13,7 @@
             <form action="{{ route('report_ads.notify', $reportAd->ad->user->id) }}" method="POST">
                 @csrf
                 <div class="modal-body" style="text-align: center">
-                    <h3> {{ __('admin_dashboard/report/messages.Do you want to block the advertising user?') }}  <span style="color: red">{{ $reportAd->ad->user->name }}</span></h3>
+                    <h3> {{ __('admin_dashboard/report/messages.Advertiser warning message?') }}  <span style="color: red">{{ $reportAd->ad->user->name }}</span></h3>
                 </div>
                 <div class="modal-body">
                     <label>  {{ __('admin_dashboard/report/messages.User newsletter') }}   </label>
@@ -21,7 +21,7 @@
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">{{ __('admin_dashboard/report/messages.close') }}</button>
-                    <button type="submit" class="btn btn-danger">{{ __('admin_dashboard/report/messages.Block the advertiser user') }} </button>
+                    <button type="submit" class="btn btn-danger">{{ __('admin_dashboard/report/messages.Advertiser warning message') }} </button>
                 </div>
             </form>
 
