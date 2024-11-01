55594416a92ea1e5b2cbe665eaad232765d7f511
Ahmed Ali Abdulghani <92600564+Ahmed2021Ali@users.noreply.github.com>
2024-11-01 22:54:49 +0200

    ssssssssss
    sssssssssssssssssss

--- app/Http/Controllers/web/NotifyController.php
+++ app/Http/Controllers/web/NotifyController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\web;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class NotifyController extends Controller
 {
-    public function notify()
+    public function notify(Request $request,$user_id)
     {
+        dd($request->reason,$user_id);
 
     }
 }
