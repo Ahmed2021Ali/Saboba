d4f1507aab8baac65dd4c399e9d35d5fb09a0ae1
Ahmed Ali Abdulghani <92600564+Ahmed2021Ali@users.noreply.github.com>
2024-11-01 19:55:07 +0200

    ssssssssssss

--- resources/views/dashboard/report_comments/notify_user_comment.blade.php
+++ resources/views/dashboard/report_comments/notify_user_comment.blade.php
@@ -1,5 +1,5 @@
 <!-- Delete Category Modal -->
-<div class="modal fade" id="notify_user_comment{{ $reportAd->id }}" tabindex="-1" role="dialog" aria-labelledby="deleteCategoryModalLabel" aria-hidden="true">
+<div class="modal fade" id="notify_user_comment{{ $reportComment->id }}" tabindex="-1" role="dialog" aria-labelledby="deleteCategoryModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
 
@@ -10,7 +10,7 @@
                 </button>
             </div>
 
-            <form action="{{ route('report_ads.notify', $reportAd->ad->user->id) }}" method="POST">
+            <form action="{{ route('report_ads.notify', $reportComment->comment->user->id) }}" method="POST">
                 @csrf
                 <div class="modal-body" style="text-align: center">
                     <h3> {{ __('admin_dashboard/report_comments/messages.Warning message to the commenter') }}  <span style="color: red">{{ $reportAd->ad->user->name }}</span></h3>
