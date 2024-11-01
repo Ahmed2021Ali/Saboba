2e27437127efc0dfc896d3f76248d57276b599ea
Ahmed Ali Abdulghani <92600564+Ahmed2021Ali@users.noreply.github.com>
2024-11-01 13:46:16 +0200

    Update messages.php

--- resources/lang/ar/admin_dashboard/report/messages.php
+++ resources/lang/ar/admin_dashboard/report/messages.php
@@ -11,5 +11,9 @@ return [
     'Advertiser warning message' => 'تحذير مستخدم الاعلات',
     'Notification to the complainant' => 'اشعار لمرسل الشكوي ',
     'No reports' => ' لا يوجد ابلاغات  ',
-
+//    delete Ads
+    'Are you sure you want to delete the ad?' => 'هل تريد حذف الاعلان ؟ ',
+    'Do you want to block the advertising user?' => ' هل تريد حظر مستخدم الاعلانات ',
+    'User newsletter' => 'رسالة اخبارية للمستخدم ',
+    'close' => 'اغلاق',
 ];
